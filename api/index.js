af4898924dcb257de65d40c4cd7cae46cc11b566
Miguel <65084675+Miguelrl17@users.noreply.github.com>
2024-09-03 19:03:25 -0700

    fixed bugs with nodemon not working, also impleted differet version when logged in v out

--- api/index.js
+++ api/index.js
@@ -6,9 +6,12 @@ const app = express();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const privateKey = '234950vsdiaojkl2jdklfdjsa;adfsf2e121';
+const cookieParser = require('cookie-parser');
 
-app.use(cors({credentials:true, origin:"http://localhost:3000"}));
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use(cookieParser());
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -34,10 +37,9 @@ app.post('/register', async (req, res) => {
 app.post("/login", async (req, res) => {
     const { username, password } = req.body;
     const userDoc = await User.findOne({ username });
-    res.json(userDoc);
     const passValid = bcrypt.compareSync(password, userDoc.password);
     if (passValid) {
-        jwt.sign({ username, id: userDoc._id, id }, privateKey, {}, (err, token) => {
+        jwt.sign({ username, id: userDoc._id }, privateKey, {}, (err, token) => {
             if (err) throw err;
             res.cookie('token', token).json("ok")
         });
@@ -50,4 +52,13 @@ app.post("/login", async (req, res) => {
 // mongodb+srv://miguelruizlicea:s63FBpFk6mqbBqCD@cluster0.uicfb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 //We are @ 1 hour 2 minutes
 // Restart Yarn and Nodemon
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, privateKey, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
 app.listen(4000);
